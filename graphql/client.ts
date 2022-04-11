6269416462044f3ce75ee44425b19fa37df6a027
Ralph Largo <devlargs@gmail.com>
2022-04-11 09:18:21 +0800

    fix: graphql endpoint

--- graphql/client.ts
+++ graphql/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/react-hooks';
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://graphql-movies-api.herokuapp.com/graphql',
 });
 
 export default client;
