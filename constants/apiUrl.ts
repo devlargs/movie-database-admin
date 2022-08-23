555119b8a70bf8c064891296c0e5e8f2b3d59310
Ralph Largo <devlargs@gmail.com>
2022-08-23 15:49:57 +0800

    feat: upload functionality

--- constants/apiUrl.ts
+++ constants/apiUrl.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://largs-movie-database-api.herokuapp.com';
-export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://largs-movie-database-api.herokuapp.com';
+// export const API_URL = 'http://localhost:4000';
--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -1,29 +1,103 @@
-import { useQuery } from '@apollo/react-hooks';
-import { Box, chakra, Flex } from '@chakra-ui/react';
+import { gql, useLazyQuery, useMutation } from '@apollo/react-hooks';
+import { Box, Flex, Heading, Spinner } from '@chakra-ui/react';
+import Container from '@components/Container';
+import UploadFile from '@components/UploadFile';
 import { GET_MOVIE_BY_ID } from '@graphql/queries/movie.query';
+import { useUploadFile } from '@store/useUploadFile';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 const MovieById: FC = () => {
+  const uploadFile = useUploadFile((e) => e.uploadFile);
   const router = useRouter();
-  const { data } = useQuery(GET_MOVIE_BY_ID, {
-    variables: {
-      id: router.query.id,
-    },
-  });
+  // const { data } = useQuery(GET_MOVIE_BY_ID, {
+  //   variables: {
+  //     id: router.query.id,
+  //     skip: !router.query.id,
+  //   },
+  // });
+
+  const [getMovie, { data }] = useLazyQuery(GET_MOVIE_BY_ID);
+
+  useEffect(() => {
+    if (router.query.id) {
+      void getMovie({
+        variables: {
+          id: router.query.id,
+        },
+      });
+    }
+  }, [router.query.id, getMovie]);
+
+  const [updateMovie, { loading }] = useMutation(gql`
+    mutation updateImage($id: String!, $imageUrl: String!) {
+      updateMovie(_id: $id, input: { imageUrl: $imageUrl }) {
+        title
+        imageHashUrl
+        actors {
+          _id
+          lastName
+        }
+      }
+    }
+  `);
 
   return (
-    <Box p={20}>
-      <chakra.h1 mb={3}>{data?.movie.title}</chakra.h1>
-      <input type="file" />
-      <br />
-      <br />
-      <Flex>
-        <img src={data?.movie.imageUrl} alt="poster" width={400} height={200} />
-        <Box w={10} />
-        {data?.movie?.imageHashUrl && <img src={data.movie.imageHashUrl} width={400} height={200} />}
+    <Container>
+      <Flex alignItems="center" justifyContent="space-between">
+        <Heading>{data?.movie.title}</Heading>
       </Flex>
-    </Box>
+
+      {/* dev part */}
+      <Box mt="20">
+        <UploadFile
+          label="upload"
+          callback={async (): Promise<void> => {
+            await uploadFile('movies', async (imageUrl) => {
+              // console.log(imageUrl);
+              await updateMovie({
+                variables: {
+                  id: data?.movie._id,
+                  imageUrl: imageUrl,
+                },
+                onCompleted: () => {
+                  void getMovie({
+                    variables: {
+                      id: router.query.id,
+                    },
+                  });
+                },
+              });
+            });
+          }}
+        />
+        <h1>dev part</h1>
+        <br />
+        <br />
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Flex>
+            {data?.movie.imageHashUrl && (
+              <Image
+                src={data.movie.imageUrl}
+                placeholder="blur"
+                width={400}
+                height={200}
+                blurDataURL={data.movie.imageHashUrl}
+                alt="tanga"
+              />
+            )}
+            <Box w={10} />
+            <img src={data?.movie.imageUrl} alt="poster" width={400} height={200} />
+            <Box w={10} />
+            {data?.movie?.imageHashUrl && <img src={data.movie.imageHashUrl} width={400} height={200} />}
+          </Flex>
+        )}
+      </Box>
+      <Box h="20" />
+    </Container>
   );
 };
 
