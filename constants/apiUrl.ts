3ede425972982f7dd5d42dd483cb559d4bb68089
Ralph Largo <devlargs@gmail.com>
2022-08-23 23:44:44 +0800

    feat: setup apiurl based on environment

--- constants/apiUrl.ts
+++ constants/apiUrl.ts
@@ -1,3 +1,3 @@
-export const API_URL = !process.env.NEXT_PUBLIC_ENVIRONMENT
+export const API_URL = process.env.NEXT_PUBLIC_ENVIRONMENT
   ? 'http://localhost:4000'
   : 'https://largs-movie-database-api.herokuapp.com';
